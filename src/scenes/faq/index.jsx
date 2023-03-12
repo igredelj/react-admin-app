57f4681cbc6e9db2488c4ebd7e630d204566eee1
igredelj <igredelj2@gmail.com>
2023-03-12 13:43:23 +0100

    inital commit

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,75 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
+
+const FAQ = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return <Box m="20px">
+        <Header title="FAQ" subtitle="Frequently asked Questions Page" />
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]}  variant="h5">
+                    An important question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Fusce commodo justo eu mattis porttitor.
+                    Duis aliquam sapien ut augue rhoncus, id lacinia arcu luctus.
+                    Praesent viverra eros vel vestibulum bibendum.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]}  variant="h5">
+                    Another important question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]}  variant="h5">
+                    Random question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Fusce commodo justo eu mattis porttitor.
+                    Duis aliquam sapien ut augue rhoncus, id lacinia arcu luctus.
+                    Praesent viverra eros vel vestibulum bibendum.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]}  variant="h5">
+                    Final question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Fusce commodo justo eu mattis porttitor.
+                    Duis aliquam sapien ut augue rhoncus, id lacinia arcu luctus.
+                    Praesent viverra eros vel vestibulum bibendum.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+    </Box>
+}
+
+export default FAQ;
